aba0c86caadf9a0076fa1147fec50153f4c35ab4
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-25 19:51:50 +0000

    automatically backed up by learn

--- db/migrate/20171225194715_create_dogs.rb
+++ db/migrate/20171225194715_create_dogs.rb
@@ -1,11 +1,7 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
+  def change
     create_table :dogs do |t|
       t.string :name
       t.string :breed
-end
-
-def down
-    drop_table :dogs
-end
+  end
 end
